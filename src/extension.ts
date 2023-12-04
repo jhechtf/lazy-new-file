4fa1c4fb88a034519cae0852e8d3773247661fe8
Jim Burbridge <jhechtf@gmail.com>
2023-12-03 20:58:58 -0800

    feat: Add in aliases

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode';
 import lazyNewFile from "./commands/lazy-new-file";
 import { configListener } from "./config";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
   // Push the configuration listener.
